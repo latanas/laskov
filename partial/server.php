35af1ad60870149fae19e511f4d9138da3bc9aad
Atanas Laskov <latanas@gmail.com>
2024-06-27 16:07:37 +0100

    Update script versions.

--- partial/server.php
+++
@@ -1,5 +0,0 @@
-<!--<small>
-    Server:
-    <strong><a href="http://www.laskov.co.uk">UK</a></strong>
-    <a href="http://www.atanaslaskov.com">US</a>
-</small>-->
--- scripts.php
+++ scripts.php
@@ -1,23 +1,19 @@
 <?php
 wp_deregister_script('jquery');
-wp_register_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js");
+wp_register_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js");
 
 wp_enqueue_script('jquery');
 wp_enqueue_script("interaction", get_template_directory_uri()."/js/interaction.js", array("jquery"), "1.8");
 wp_enqueue_script("main", get_template_directory_uri()."/js/main.js", array("jquery", "interaction"), "1.5");
-/*wp_enqueue_script("gumroad", "https://gumroad.com/js/gumroad.js");*/
 
 wp_enqueue_style("archivo_narrow", "https://fonts.googleapis.com/css?family=Roboto+Slab");
-wp_enqueue_style("style", get_template_directory_uri()."/style.css", array(), "6.2");
+wp_enqueue_style("style", get_template_directory_uri()."/style.css", array(), "6.3");
 
 wp_oembed_add_provider( '#https?://sketchfab\.com/.*#i', 'https://www.sketchfab.com/oembed', true );
 
-/* Remove Gutenberg Block Library CSS from loading on the frontend
-  html :where(img) { height: auto; max-width: 100%; } */
-
-function smartwp_remove_wp_block_library_css(){
+/* Remove Gutenberg Block Library CSS from loading on the frontend, and other default CSS that this theme doesn't use */
+add_action( 'wp_enqueue_scripts', function() {
     wp_dequeue_style( 'wp-block-library' );
     wp_dequeue_style( 'wp-block-library-theme' );
-    wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
-} 
-add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+    wp_dequeue_style( 'wc-blocks-style' );
+}, 100 );

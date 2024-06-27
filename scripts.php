bffdde43759da9a58e17f30d9ef2d743b68edfec
Atanas Laskov <latanas@gmail.com>
2024-06-27 16:21:15 +0100

    Remove sketchfab script.

--- scripts.php
+++ scripts.php
@@ -7,9 +7,7 @@ wp_enqueue_script("interaction", get_template_directory_uri()."/js/interaction.j
 wp_enqueue_script("main", get_template_directory_uri()."/js/main.js", array("jquery", "interaction"), "1.5");
 
 wp_enqueue_style("archivo_narrow", "https://fonts.googleapis.com/css?family=Roboto+Slab");
-wp_enqueue_style("style", get_template_directory_uri()."/style.css", array(), "6.3");
-
-wp_oembed_add_provider( '#https?://sketchfab\.com/.*#i', 'https://www.sketchfab.com/oembed', true );
+wp_enqueue_style("style", get_template_directory_uri()."/style.css", array(), "6.4");
 
 /* Remove Gutenberg Block Library CSS from loading on the frontend, and other editor CSS that this theme doesn't use */
 add_action( 'wp_enqueue_scripts', function() {

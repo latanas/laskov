2b0cfab491f01b909cf1e2c8838da3f675058204
Atanas Laskov <latanas@gmail.com>
2024-12-01 07:56:35 +0000

    Update illustrations portfolio.

--- partial/artwork.php
+++ partial/artwork.php
@@ -11,42 +11,6 @@
 by <strong>Atanas Laskov</strong>
 </header>
 
-<h3>Tree of Life Illustrations</h3>
-
-<p>
-I made the following illustrations in Adobe Photoshop and InDesign. 
-The Tree of Life contains ten planetary spheres known as Sephirot. 
-Each sphere corresponds to some ideal quality, or aspect of the Cosmos.
-</p>
-<p>
-Legend has it that the Tree of Life was revealed to Moses by a priest at the royal court of Egypt.
-It is an important part of the ancient Qabalah.
-This diagram was used by the Renaissance pholsophpers, such as Cornelius Agrippa,
-who studied the Qabalah and combined it with Platonic idealism and Graeco-Roman-Egyptian culture.
-Pico Mirandola believed that Qabalah can unify all the different classical cultures.
-</p>
-
-<!--Each illustration corresponds to some Platonic ideal, for example "Strenght", "Passion", "Imagination".
-There are 10 ideal spheres, known as Sephirot, and together they are arranged in the "Tree of Life".-->
-
-<p>
-  <?php
-   $folder="color";
-   template_illustration("ALAskov_Sephirot10_1100.jpg", 54, 46, 0);
-   template_illustration("ALaskov_Sephirot09_1100.jpg", 52, 56, 0);
-   template_illustration("ALaskov_Sephirot08_1100.jpg", 43, 37, 0);
-   template_illustration("ALaskov_Sephirot07_1100.jpg", 56, 50, 0);
-   template_illustration("ALaskov_Sephirot06_1100.jpg", 43, 56, 0);
-   template_illustration("ALaskov_Sephirot05_1100.jpg", 32, 47, 0);
-   template_illustration("ALaskov_Sephirot04_1100.jpg", 51, 50, 0);
-   template_illustration("ALaskov_Sephirot03_1100.jpg", 56, 40, 0);
-   template_illustration("ALaskov_Sephirot02_1100.jpg", 33, 30, 0);
-   template_illustration("ALaskov_Sephirot01_1100.jpg", 25, 32, 0);
-   /*template_illustration("ALaskov_TreeOfLife_Infographic_Colonna1100.jpg", 73, 95, 0);*/
-   
-?>
-</p>
-
 <h3 id="gn">Graphic Novels</h3>
 <p>
 I published 3 volumes of the graphic novel "Wings of Steel". This is a complete fantasy story with 125 illustrations.
@@ -89,28 +53,8 @@ In order to draw character designs, human anatomy is an important topic for arti
   $folder = "bw";
   template_illustration("laskov_master_study_009_proportions_a.jpg", 50, 8);
   template_illustration("laskov_ultra_simplified_front_047_port.jpg", 9, 14);
+  template_illustration("ALaskov_SkullLow.jpg", 6, 30);
   ?>
-</p>
-
-<h3 id="anatomy">2D Animations</h3>
-
-<p>My animated 2D work is produced in Adobe Animate, and with the grease pencil in Blender.</p>
-
-<p>
-  <?php
-   $folder="color";
-
-   template_illustration("ALAskov_Monster1100.gif", 45, 40, 0);
-   template_illustration("ALaskov_Girl1100.gif", 53, 21, 0);
-   template_illustration("LaskovTeapotAnim.gif", 65, 13, 0);
-   template_illustration("LaskovSpriteAnim.gif", 55, 40, 0);
-   template_illustration("LaskovFlameAnim.gif", 50, 70, 0);
-   template_illustration("LaskovRavenAnim.gif", 50, 40, 0);
-   template_illustration("LaskovCompassAnim.gif", 50, 50, 0);
-   template_illustration("LaskovTempleAnim.gif", 36, 76, 0);
-   /*template_illustration("ALaskov_Boxer_Showreel_16_9.gif", 63, 89, 0);
-   template_video_poster("ALaskov_Boxer_Showreel_16_9.gif","ALaskov_Boxer_Showreel_16_9", "ALaskov_Boxer_Showreel_16_9", 170);  */
-?>
 </p>
 
 <h3>Character Designs</h3>
@@ -188,11 +132,11 @@ I strive for creativity and technical excellence in my artwork.
 
   
   /*template_illustration("laskov_persp_1100.jpg", 19, 12);
-  template_illustration("laskov_env_001.jpg", 59,39,0);
+  template_illustration("laskov_env_001.jpg", 59,39,0);*/
 
   $folder="color";
   template_illustration("Daphne_color_1100.jpg", 53,20);
-  template_illustration("Laskov_TheRedQueen_1100.png", 11,7,0);*/
+  template_illustration("Laskov_TheRedQueen_1100.png", 11,7,0);
 
   ?>
 </p>
@@ -214,5 +158,60 @@ I strive for creativity and technical excellence in my artwork.
    template_illustration("LaskovBoat.jpg", 85, 8, 0);
    template_illustration("Composition.jpg", 86, 23, 0);
 ?>
-</p>-->
+</p>
 
+
+<h3>Tree of Life Illustrations</h3>
+
+<p>
+I made the following illustrations in Adobe Photoshop and InDesign. 
+The Tree of Life contains ten planetary spheres known as Sephirot. 
+Each sphere corresponds to some ideal quality, or aspect of the Cosmos.
+</p>
+<p>
+Legend has it that the Tree of Life was revealed to Moses by a priest at the royal court of Egypt.
+It is an important part of the ancient Qabalah.
+This diagram was used by the Renaissance pholsophpers, such as Cornelius Agrippa,
+who studied the Qabalah and combined it with Platonic idealism and Graeco-Roman-Egyptian culture.
+Pico Mirandola believed that Qabalah can unify all the different classical cultures.
+</p>
+
+<p>
+  <?php
+   $folder="color";
+   template_illustration("ALAskov_Sephirot10_1100.jpg", 54, 46, 0);
+   template_illustration("ALaskov_Sephirot09_1100.jpg", 52, 56, 0);
+   template_illustration("ALaskov_Sephirot08_1100.jpg", 43, 37, 0);
+   template_illustration("ALaskov_Sephirot07_1100.jpg", 56, 50, 0);
+   template_illustration("ALaskov_Sephirot06_1100.jpg", 43, 56, 0);
+   template_illustration("ALaskov_Sephirot05_1100.jpg", 32, 47, 0);
+   template_illustration("ALaskov_Sephirot04_1100.jpg", 51, 50, 0);
+   template_illustration("ALaskov_Sephirot03_1100.jpg", 56, 40, 0);
+   template_illustration("ALaskov_Sephirot02_1100.jpg", 33, 30, 0);
+   template_illustration("ALaskov_Sephirot01_1100.jpg", 25, 32, 0);
+   /*template_illustration("ALaskov_TreeOfLife_Infographic_Colonna1100.jpg", 73, 95, 0);*/
+   
+?>
+</p>
+
+
+<h3 id="anatomy">2D Animations</h3>
+
+<p>My animated 2D work is produced in Adobe Animate, and with the grease pencil in Blender.</p>
+
+<p>
+  <?php
+   $folder="color";
+
+   template_illustration("ALAskov_Monster1100.gif", 45, 40, 0);
+   template_illustration("ALaskov_Girl1100.gif", 53, 21, 0);
+   template_illustration("LaskovTeapotAnim.gif", 65, 13, 0);
+   template_illustration("LaskovSpriteAnim.gif", 55, 40, 0);
+   template_illustration("LaskovFlameAnim.gif", 50, 70, 0);
+   template_illustration("LaskovRavenAnim.gif", 50, 40, 0);
+   template_illustration("LaskovCompassAnim.gif", 50, 50, 0);
+   template_illustration("LaskovTempleAnim.gif", 36, 76, 0);
+   /*template_illustration("ALaskov_Boxer_Showreel_16_9.gif", 63, 89, 0);
+   template_video_poster("ALaskov_Boxer_Showreel_16_9.gif","ALaskov_Boxer_Showreel_16_9", "ALaskov_Boxer_Showreel_16_9", 170);  */
+?>
+</p>-->
